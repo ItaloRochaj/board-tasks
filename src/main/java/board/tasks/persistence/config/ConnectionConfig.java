84e5c2f5cdfaf14b0faba9330ffbe26268c34b79
Italo Rocha <italorochaj@icloud.com>
2025-02-26 11:45:18 -0300

    Criado classe de conexão com o Mysql

---
+++ src/main/java/board/tasks/persistence/config/ConnectionConfig.java
@@ -0,0 +1,21 @@
+package board.tasks.persistence.config;
+
+import lombok.NoArgsConstructor;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@NoArgsConstructor(access = PRIVATE)
+public final class ConnectionConfig {
+
+    public static Connection getConnection() throws SQLException {
+        var url = "jdbc:mysql://localhost/board";
+        var user = "italo";
+        var password = "japa@20";
+        var connection = DriverManager.getConnection(url, user, password);
+        connection.setAutoCommit(false);
+        return connection;
+    }
+}
