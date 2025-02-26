0774c98f4973645a3a03e910edcf657f204625ee
Italo Rocha <italorochaj@icloud.com>
2025-02-26 18:39:27 -0300

    Implementado BoardService

--- src/main/java/board/tasks/service/BoardService.java
+++ src/main/java/board/tasks/service/BoardService.java
@@ -1,6 +1,7 @@
 package board.tasks.service;
 
 import board.tasks.persistence.dao.BoardDAO;
+import board.tasks.persistence.entity.BoardEntity;
 import lombok.AllArgsConstructor;
 
 import java.sql.Connection;
@@ -10,6 +11,19 @@ import java.sql.SQLException;
 public class BoardService {
 
     private final Connection connection;
+
+    public BoardEntity insert(final BoardEntity entity) throws SQLException {
+        var dao = new BoardDAO(connection);
+        try{
+            dao.insert(entity);
+        } catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+        return entity;
+
+    }
+
     public boolean delete(final Long id) throws SQLException{
 
         var dao = new BoardDAO(connection);
